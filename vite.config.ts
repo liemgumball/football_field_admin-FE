eee7d1825c9b934ba896d28625bbb0e26ac2c8f0
liemgumball <liem1762001@gmail.com>
2024-05-15 16:44:22 +0700

    feat: code setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react(), tsConfigPaths()],
+	publicDir: 'src/assets',
+	build: {
+		outDir: 'dist',
+	},
 })
