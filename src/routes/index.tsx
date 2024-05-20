a139211118c1babe261451d607f71f54591428e0
liemgumball <liem1762001@gmail.com>
2024-05-20 14:59:39 +0700

    feat: suspense routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,19 +5,20 @@ import {
 } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import Layout from '@/components/Layout'
+import { lazy } from 'react'
 
-import Login from '@/pages/Login'
-import NotFound from '@/pages/NotFound'
-import Dashboard from '@/pages/Dashboard'
-import Home from '@/pages/Home'
+const Login = lazy(() => import('@/pages/Login'))
+const NotFound = lazy(() => import('@/pages/NotFound'))
+const Dashboard = lazy(() => import('@/pages/Dashboard'))
+const Home = lazy(() => import('@/pages/Home'))
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route element={<Layout />}>
 			<Route path="/login" element={<Login />} />
-			<Route path="*" element={<NotFound />} />
 			<Route path="/" element={<PrivateRoute />}>
 				<Route element={<Dashboard />}>
+					<Route path="*" element={<NotFound />} />
 					<Route index element={<Home />} />
 				</Route>
 			</Route>
