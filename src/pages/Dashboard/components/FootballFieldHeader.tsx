672b1b65f9cf6140dfd6679c0b2939521af8f7d7
liemgumball <liem1762001@gmail.com>
2024-05-23 09:41:41 +0700

    feat: refresh query & tooltip

--- src/pages/Dashboard/components/FootballFieldHeader.tsx
+++ src/pages/Dashboard/components/FootballFieldHeader.tsx
@@ -1,18 +1,44 @@
 import { Icons } from '@/components/Icons'
 import { Button } from '@/components/ui/button'
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { TFootballFieldContext } from '@/types'
+import { useQueryClient } from '@tanstack/react-query'
 
 const FootballFieldHeader = (props: TFootballFieldContext) => {
-	const { field, refetch, isFetching, isStale } = props
+	const { field, isFetching, isStale } = props
 
-	if (!field || !refetch) return null
+	const queryClient = useQueryClient()
+
+	const onClick = () => {
+		queryClient.invalidateQueries()
+	}
 
 	return (
 		<section className="flex items-center justify-between gap-4">
 			<h2>{field.name}</h2>
-			<Button onClick={refetch} size="sm" disabled={isStale} variant="ghost">
-				<Icons.Refresh isFetching={isFetching} />
-			</Button>
+			<TooltipProvider>
+				<Tooltip>
+					<Button
+						onClick={onClick}
+						size="sm"
+						disabled={!isStale}
+						variant="ghost"
+						asChild
+					>
+						<TooltipTrigger>
+							<Icons.Refresh isFetching={isFetching} />
+						</TooltipTrigger>
+					</Button>
+					<TooltipContent>
+						<p>Refresh</p>
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
 		</section>
 	)
 }
