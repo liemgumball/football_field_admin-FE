d48b1d42686d4c561f6f67e6b6da803af48c6951
liemgumball <liem1762001@gmail.com>
2024-06-19 05:40:17 +0700

    fix: sheet content side

--- src/pages/SubFieldDetails/components/SubFieldEditSheet.tsx
+++ src/pages/SubFieldDetails/components/SubFieldEditSheet.tsx
@@ -23,7 +23,7 @@ const SubFieldEditSheet = ({ subfield }: { subfield: TSubField }) => {
 					<PencilIcon size={16} />
 				</Button>
 			</SheetTrigger>
-			<SheetContent side="top">
+			<SheetContent side="right">
 				<SheetHeader>
 					<SheetTitle className="text-2xl font-bold">Edit SubField</SheetTitle>
 					<SheetDescription className="text-base font-medium">
